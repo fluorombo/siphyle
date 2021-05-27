f533d27ef166f40b4eb74d12ec4b31156c3a249d
Neptunit <52449983+neptunit@users.noreply.github.com>
2021-05-27 16:56:29 +0200

    Add files via upload

---
+++ file.js
@@ -0,0 +1,20 @@
+var count = 0
+var string = ""
+/*
+var intervalObject = setInterval(function () {
+    string = ""
+    if (count > 50) {
+        count = 0
+    }
+
+    for (i = count; i != 0; i--) {
+        string += "*"
+    }
+
+    count += 1
+    console.clear() 
+    console.log(string)
+
+    }, 20); */
+    console.log("ciao")
+    console.log("oh")
